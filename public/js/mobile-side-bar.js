3fce56dcfe3a6d23850b59899ba813241ebc5515
Jinsup9k <01077717141p@gmail.com>
2020-06-16 18:55:52 +0900

    mobile sidebar modified

--- public/js/mobile-side-bar.js
+++ public/js/mobile-side-bar.js
@@ -1,24 +1,22 @@
 $(document).ready(function () {
   $(".menu-icon-div").click(function () {
-    if ($(this).length > 0) {
-      setTimeout(function () {
-        $(".sidebar").css("left", "0");
-        $(".sidebar").css(
-          "background",
-          ' url("/public/img/ss1.jpg") #006600 no-repeat'
-        );
-        $(".sidebar").css("background-size", "100% 19rem");
-        $(".menu-icon-div").css("display", "none");
-      }, 200);
+    if ($(".sidebar").css("left") == "-250px") {
+      $(".sidebar").animate({ width: "show", left: "0" }, 200, function () {});
+      //   $(".menu-icon-div").css("display", "none");
     } else {
-      return false;
+      $(".sidebar").animate(
+        { width: "hide", left: "-250px" },
+        200,
+        function () {}
+      );
+      $(".menu-icon-div").css("color", "#797979");
     }
   });
 
   $(".content").click(function () {
-    setTimeout(function () {
-      $(".sidebar").css("left", "-250px");
-      $(".menu-icon-div").css("display", "block");
-    }, 200);
+    if ($(".menu-icon-div").length > 0) {
+      $(".sidebar").animate({ width: "toggle" }, 200, function () {});
+      //   $(".menu-icon-div").css("display", "block");
+    }
   });
 });
