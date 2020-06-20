8b699a0f8e5253afc99c36a9c8bbbb1c60146a1a
Jinsup9k <01077717141p@gmail.com>
2020-06-20 18:04:14 +0900

    theme update 1

--- public/js/mobile-side-bar.js
+++ public/js/mobile-side-bar.js
@@ -4,19 +4,15 @@ $(document).ready(function () {
       $(".sidebar").animate({ width: "show", left: "0" }, 200, function () {});
       //   $(".menu-icon-div").css("display", "none");
     } else {
-      $(".sidebar").animate(
-        { width: "hide", left: "-250px" },
-        200,
-        function () {}
-      );
+      $(".sidebar").animate({ left: "-250px" }, 200, function () {});
       $(".menu-icon-div").css("color", "#797979");
     }
   });
 
-  $(".content").click(function () {
-    if ($(".menu-icon-div").length > 0) {
-      $(".sidebar").animate({ width: "toggle" }, 200, function () {});
-      //   $(".menu-icon-div").css("display", "block");
-    }
-  });
+  // $(".content").click(function () {
+  //   if ($(".menu-icon-div").length > 0) {
+  //     $(".sidebar").animate({ width: "toggle" }, 200, function () {});
+  //     //   $(".menu-icon-div").css("display", "block");
+  //   }
+  // });
 });
